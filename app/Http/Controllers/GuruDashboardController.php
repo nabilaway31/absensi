3a5e3f3df9c96c87e13d100977b19ac3b6b79962
nabilaway31 <nabilaiswanti21@gmail.com>
2026-01-16 23:32:03 +0700

    update tampilan user

--- app/Http/Controllers/GuruDashboardController.php
+++ app/Http/Controllers/GuruDashboardController.php
@@ -23,13 +23,7 @@ class GuruDashboardController extends Controller
             ->where('tanggal', $hariIni)
             ->first();
 
-<<<<<<< HEAD
-        // KIRIM SEMUA KE VIEW
-=======
-        // Waktu server sekarang untuk inisialisasi tampilan
-        $jamSekarang = Carbon::now()->format('H:i:s');
-
->>>>>>> bdd3300716baf51c08f4567ac1e83e08743c750d
+        // Kirim ke view
         return view('guru.dashboard', compact(
             'guru',
             'absensiHariIni',
